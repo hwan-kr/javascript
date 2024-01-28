d618eac0cfdf47b510409e73fcc22fa11bdf6ee3
hwan-kr <fabronjeon@naver.com>
2024-01-29 07:28:58 +0900

    24-01-29 07:28 Lecture 125 complete

--- section4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ section4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -11,14 +11,27 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('Maximum life for you and the moster.', '100');
-
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+function getMaxLifeValues() {
+    const enteredValue = prompt('Maximum life for you and the moster.', '100');
+
+    const parsedValue = parseInt(enteredValue);
+    if (isNaN(parsedValue) || parsedValue <= 0) {
+        throw { message: 'Invaild user input, Not a number!' };
+    }
+    return parsedValue;
+}
+
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
     chosenMaxLife = 100;
+    alert('YOu entered something wrong. default value of 100 was used.');
 }
 
 let currentMonsterHealth = chosenMaxLife;

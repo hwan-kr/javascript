8f17552c596af92677e435279ae4eb4af1943c41
hwan-kr <fabronjeon@naver.com>
2024-01-30 07:04:50 +0900

    24-01-30 07:04 Lecture 133 complete

--- section6/functions-01-starting-code/app.js
+++ section6/functions-01-starting-code/app.js
@@ -1,15 +1,66 @@
-const startGameBtn = document.getElementById("start-game-btn");
+const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log("Game is starting...");
-}
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
-const person = {
-    greet: function greet() {
-        console.log("Hello there!");
-    },
+let gameIsRunning = false;
+
+let whatComputerChoice = [];
+
+const getPlayerChoice = function () {
+    const selection = prompt(
+        `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+        ''
+    ).toUpperCase();
+    if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+        alert('Invalid choice! We chose Rock for you!');
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
 };
 
-person.greet();
+const getComputerChoice = function () {
+    const randomValue = Math.random();
+    if (randomValue < 0.34) {
+        whatComputerChoice.push(ROCK);
+        return ROCK;
+    } else if (randomValue < 0.76) {
+        whatComputerChoice.push(PAPER);
+        return PAPAER;
+    } else {
+        whatComputerChoice.push(SCISSORS);
+        return SCISSORS;
+    }
+};
 
-startGameBtn.addEventListener("click", startGame);
+const getWinner = function (cChoice, pChoice) {
+    if (cChoice === pChoice) {
+        return RESULT_DRAW;
+    } else if (
+        (cChoice === ROCK && pChoice === PAPER) ||
+        (cChoice === SCISSORS && pChoice === ROCK) ||
+        (cChoice === PAPER && pChoice === SCISSORS)
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
+startGameBtn.addEventListener('click', function () {
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(whatComputerChoice);
+    console.log(winner);
+});
